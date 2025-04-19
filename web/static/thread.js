62b91135095f37b9e05a0f916807ea5bb038d2ce
Dominic Ferrando <dominicf2001@gmail.com>
2025-04-19 19:21:06 -0400

    Img full/thumb toggling. Small tweaks

--- web/static/thread.js
+++ web/static/thread.js
@@ -1,34 +1,51 @@
-const posts = Array.from(document.querySelectorAll("article"));
+function insertHeaderReplies() {
+    const posts = Array.from(document.querySelectorAll("article"));
+    const replies = {};
 
-const replies = {};
+    for (const post of posts) {
+        const postId = +post.id.split("-")[1];
+        const postBody = post.querySelector("p").textContent;
 
-for (const post of posts) {
-    const postId = +post.id.split("-")[1];
-    const postBody = post.querySelector("p").textContent;
+        const replyLinkIds = [...postBody.matchAll(/(?:^|\s)>>(\d+)\b/g)].map(m => parseInt(m[1]));
+        for (const replyLinkId of replyLinkIds) {
+            replies[replyLinkId] = replies[replyLinkId] ?
+                replies[replyLinkId].add(postId) :
+                replies[replyLinkId] = new Set([postId]);
+        }
+    }
 
-    const replyLinkIds = [...postBody.matchAll(/(?:^|\s)>>(\d+)\b/g)].map(m => parseInt(m[1]));
-    for (const replyLinkId of replyLinkIds) {
-        replies[replyLinkId] = replies[replyLinkId] ?
-            replies[replyLinkId].add(postId) :
-            replies[replyLinkId] = new Set([postId]);
+    for (const postId in replies) {
+        const post = $(`post-${postId}`);
+        if (!post) break;
+
+        const postRepliesContainer = post.querySelector(".post-replies");
+
+        const replyIds = Array.from(replies[postId]);
+        for (const replyId of replyIds) {
+            const replyLink = document.createElement("a");
+            replyLink.textContent = `>>${replyId}`;
+            replyLink.classList.add("reply-link-header");
+            replyLink.setAttribute("href", `#post-${replyId}`);
+
+            replyLink.onmouseover = (e) => highlightPost(+replyId, e);
+            replyLink.onmouseleave = (e) => highlightPost(+replyId, e, false);
+            replyLink.onclick = onReplyLinkClick;
+
+            postRepliesContainer.append(replyLink);
+        }
     }
 }
 
-for (const postId in replies) {
-    const post = $(`post-${postId}`);
-    const postRepliesContainer = post.querySelector(".post-replies");
-
-    const replyIds = Array.from(replies[postId]);
-    for (const replyId of replyIds) {
-        const replyLink = document.createElement("a");
-        replyLink.textContent = `>>${replyId}`;
-        replyLink.classList.add("reply-link-header");
-        replyLink.setAttribute("href", `#post-${replyId}`);
-
-        replyLink.onmouseover = (e) => highlightPost(+replyId, e);
-        replyLink.onmouseleave = (e) => highlightPost(+replyId, e, false);
-        replyLink.onclick = onReplyLinkClick;
-
-        postRepliesContainer.append(replyLink);
+function togglePostImage(img) {
+    const filename = img.src.split("/").at(-1);
+    if (img.classList.contains("post-img-full")) {
+        img.classList.remove("post-img-full");
+        img.src = "/static/img/posts/thumb/" + filename;
+    }
+    else {
+        img.classList.add("post-img-full");
+        img.src = "/static/img/posts/full/" + filename;
     }
 }
+
+insertHeaderReplies()
